1874ab20e1831fc6c11cbd6591f51260bb74cbb2
Cory Piette <coryjpiette@gmail.com>
2021-06-27 12:14:47 -0400

    defining variables

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var findCityInput = $("#find-city");
+var findCityBtn = $("#find-city-button");
+var savedHistory = $('#saved-history');
+var clearDataBtn = $("#clear-data");
+var City = $("#city");
+var Temp = $("#temp");
+var Wind = $("#wind");
+var Humidity = $("#humidity");
+var UVindex = $("#uvindex");
+
