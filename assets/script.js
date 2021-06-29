f4544419ebbb5ec7c02ded34f8a942d3693546e3
Cory Piette <coryjpiette@gmail.com>
2021-06-29 08:29:57 -0400

    fixing HTML typos, formatting and adding forecast vars

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,8 @@ var Temp = $("#temp");
 var Wind = $("#wind");
 var Humidity = $("#humidity");
 var UVindex = $("#uvindex");
-var weatherData =$('#weather-data');
-var cityList=[]
+var weatherData = $('#weather-data');
+var cityList = []
 
 
 //Retrieve current date
@@ -21,89 +21,114 @@ clearHistory();
 
 
 //register city input from search hox
-$(document).on("submit", function(){
+$(document).on("submit", function () {
     event.preventDefault();
 
     var searchValue = findCityInput.val().trim();
 
     currentConditions(searchValue)
     searchHistory(searchValue);
-    findCityInput.val(""); 
+    findCityInput.val("");
 });
- 
+
 //my API key from Open Weather
 var APIkey = "d8b1de6dc1f6e8219e73d53dc5f1c2c3";
 
 //button click to add to search history
-findCityBtn.on("click", function(event){
+findCityBtn.on("click", function (event) {
     event.preventDefault();
     var searchValue = findCityInput.val().trim();
 
     currentConditions(searchValue)
-   searchHistory(searchValue);    
-   findCityInput.val(""); 
+    searchHistory(searchValue);
+    findCityInput.val("");
 });
 
 // Clearing search history
-clearHistoryButton.on("click", function(){ 
+clearHistoryButton.on("click", function () {
     cityList = [];
 
-        // Update city list history in local storage
-        listArray();
-    
-        $(this).addClass("hide");
-    });
-    
-    // Clicking button will prompt city info
-    savedHistory.on("click","li.city-btn", function(event) {
-      
-        var value = $(this).data("value");
-         currentConditions(value);
-        searchHistory(value);  
-    });
-    
+    // Update city list history in local storage
+    listArray();
+
+    $(this).addClass("hide");
+});
+
+// Clicking button will prompt city info
+savedHistory.on("click", "li.city-btn", function (event) {
+
+    var value = $(this).data("value");
+    currentConditions(value);
+    searchHistory(value);
+});
+
 // Request Open Weather API based on user input
-function  currentConditions(searchValue) {
-    
+function currentConditions(searchValue) {
+
     //AJAX API call
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + APIkey;
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-    .then(function(response){
-        console.log(response);
-        City.text(response.name);
-        City.append("<small class='text-muted' id='current-date'>");
-        $("#current-date").text("(" + currentDate + ")");
-        City.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />" )
-        Temp.text(response.main.temp);
-        Temp.append("&deg;F");
-        Humidity.text(response.main.humidity + "%");
-        Wind.text(response.wind.speed + "MPH");
+        .then(function (response) {
+            console.log(response);
+            City.text(response.name);
+            City.append("<small class='text-muted' id='current-date'>");
+            $("#current-date").text("(" + currentDate + ")");
+            City.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />")
+            Temp.text(response.main.temp);
+            Temp.append("&deg;F");
+            Humidity.text(response.main.humidity + "%");
+            Wind.text(response.wind.speed + "MPH");
 
-        var lat = response.coord.lat;
-        var long = response.coord.long;
-        
+            var lat = response.coord.lat;
+            var long = response.coord.long;
 
-        var UVurl = "https://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + long + "&appid=" + APIkey;
-        //AJAX call - UV index
-        $.ajax({
-            url: UVurl,
-            method: "GET"
-        }).then(function(response){
-            UVindex.text(response.value);
-        });
 
-        //finding country code
-        var countryCode = response.sys.country;
-        var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=" + APIkey + "&lat=" + lat +  "&lon=" + lon;
+            var UVurl = "https://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + long + "&appid=" + APIkey;
 
-        });
+            //AJAX call - UV index
+            $.ajax({
+                url: UVurl,
+                method: "GET"
+            }).then(function (response) {
+                UVindex.text(response.value);
+            });
 
-    }
+            //finding country code
+            var countryCode = response.sys.country;
+            var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=" + APIkey + "&lat=" + lat + "&lon=" + lon;
 
-    
+            // AJAX call - 5-day forecast
+            $.ajax({
+                url: forecastURL,
+                method: "GET"
+            })
+
+                .then(function (response) {
+                    console.log(response);
+                    $('#five-day-forecast').empty();
+                    for (var i = 1; i < response.list.length; i += 8) {
+
+                        var forecastDateString = moment(response.list[i].dt_txt).format("L");
+                        console.log(forecastDateString);
+
+                        var forecastCol = $("<div class='col-12 col-md-6 col-lg forecast-day mb-3'>");
+                        var forecastCard = $("<div class='card'>");
+                        var forecastCardBody = $("<div class='card-layout'>");
+                        var forecastDate = $("<h4 class='card-title'>");                 
+                        var forecastTemp = $("<p class='card-input mb-0'>");
+                        var forecastHumidity = $("<p class='card-input mb-0'>");
+                        var forecastIcon = $("<img>");
+
+
+
+                    });
+
+        }
+
+
 
 
